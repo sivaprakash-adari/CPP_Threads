bb250e90d68e3801dda78989c10854a2809bf593
Siva Prakash Adari <adari.sivaprakash@gmail.com>
2025-02-23 19:08:17 -0600

    Ignore .vscode directory

---
+++ 01_Lanching_threads.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<thread>
+
+//using namespace std;
+
+void test() {
+    std::cout << "Hello from test" << std::endl;
+}
+void functionA() {
+    std::cout << "This is function A" << std::endl;
+    std::thread threadC(test);
+    threadC.join();
+}
+
+void functionB() {
+    std::cout << "This is function B" << std::endl;
+}
+
+
+
+int main()
+{
+    std::thread threadA(functionA);
+    std::thread threadB(functionB);
+    threadA.join();
+    threadB.join();
+    return 0;
+}
